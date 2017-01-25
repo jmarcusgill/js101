d35188deab8c48df180395a1f4c1ba8d04756ef7
jmarcusgill <jmarcusgill@gmail.com>
2017-01-24 19:23:12 -0600

    completed switch statement

---
+++ day2.js
@@ -0,0 +1,15 @@
+// switch statements
+
+var day = "Friday"
+
+switch(day) {
+  case "Monday":
+    console.log("Boooooo");
+    break;
+  case "Friday":
+    console.log("PARTAYYYY")
+    break;
+  default:
+    console.log("not monday");
+}
+
