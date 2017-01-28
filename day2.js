ad33fca9e6edcabd0a8bf717ac878ff6f41ed628
jmarcusgill <jmarcusgill@gmail.com>
2017-01-28 10:44:24 -0600

    finished class assignment

--- day2.js
+++ day2.js
@@ -71,10 +71,22 @@ for (var i = 0; i < 101; i+=10){
 
 
 // for loop that pushes the index to an array for 5 times console.log (array) [0, 1, 2, 3, 4]
+var myArray = [];
+
+for (var i = 0; i < 5; i++) {
+  myArray.push(i);
+}
+
+console.log("myArray ", myArray);
 
 
 // for loop that starts by 100 and divides by 2 each time = 3 times  100,50,25
+var num = 100;
 
+for (var j = 0; j < 3; j++) {
+  console.log("number: ", num);
+  num = num / 2;
+}
 
 
 
