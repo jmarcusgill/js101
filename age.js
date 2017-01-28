7c3142471035b031261a1422b75253308f48cc16
jmarcusgill <jmarcusgill@gmail.com>
2017-01-28 10:09:57 -0600

    commented out some stuff

--- age.js
+++ age.js
@@ -1,4 +1,4 @@
-console.log("testing")
+// console.log("testing")
 
 
 var userAge = 25;
@@ -11,23 +11,23 @@ var yearsInDecade = 10;
 var daysInYear = 365;
 
 // 1. How many hours are in a year
-var hoursPerYear = hoursInDay * daysInYear;
-console.log(hoursPerYear);
+// var hoursPerYear = hoursInDay * daysInYear;
+// console.log(hoursPerYear);
 
 
-// 2. How many minutes are in a decade
-var minutesPerDecade = minutesInHour * hoursInDay * daysInYear * yearsInDecade;
-console.log(minutesPerDecade);
+// // 2. How many minutes are in a decade
+// var minutesPerDecade = minutesInHour * hoursInDay * daysInYear * yearsInDecade;
+// console.log(minutesPerDecade);
 
 
-// 3. How many seconds old they are
-var secondsOld = secondsInMinute * minutesInHour * hoursInDay * userAge;
-console.log(secondsOld);
+// // 3. How many seconds old they are
+// var secondsOld = secondsInMinute * minutesInHour * hoursInDay * userAge;
+// console.log(secondsOld);
 
 
 // 4. If they are older than some arbitrary number, console "I'm old", else "I'm young" (edited)
-if (userAge > 35) {
-  console.log("I'm old!")
-} else {
-  console.log("I'm young!")
-}
+// if (userAge > 35) {
+//   console.log("I'm old!")
+// } else {
+//   console.log("I'm young!")
+// }
--- day2.js
+++ day2.js
@@ -13,44 +13,67 @@
 //     console.log("not monday");
 // }
 
-var fruit = ["cherries", "apples", "bananas"];
-var fruitString = fruit.join(", ")
-console.log(fruitString)
+// var fruit = ["cherries", "apples", "bananas"];
+// var fruitString = fruit.join(", ")
+// console.log(fruitString)
 
-var catColors = "green,red,yellow,black";
-var catColorsArray = catColors.split(",")
-console.log(catColorsArray)
+// var catColors = "green,red,yellow,black";
+// var catColorsArray = catColors.split(",")
+// console.log(catColorsArray)
 
-var reverseFruit = fruit.reverse();
-console.log(reverseFruit)
+// var reverseFruit = fruit.reverse();
+// console.log(reverseFruit)
 
-var alphaFruit = fruit.sort();
-console.log(alphaFruit);
+// var alphaFruit = fruit.sort();
+// console.log(alphaFruit);
 
-var nums = [9,6,8,2,4,11,66]; //2,4,6,8,9,11,66
-var numSort = nums.sort(function(a,b) {return a-b}); //to sort numbers by smallest to largest
-console.log(numSort);
+// var nums = [9,6,8,2,4,11,66]; //2,4,6,8,9,11,66
+// var numSort = nums.sort(function(a,b) {return a-b}); //to sort numbers by smallest to largest
+// console.log(numSort);
 
-console.log(reverseFruit.indexOf("bananas"))
+// console.log(reverseFruit.indexOf("bananas"))
 
-var animals = ["cats", "turtles", "fish", "bears", "tigers"];
+// var animals = ["cats", "turtles", "fish", "bears", "tigers"];
 
-// sclice - returns copy of what you ask it to. First one you don't want to include.
-var animals2 = animals.slice(2,4);
-console.log(animals2)
+// // sclice - returns copy of what you ask it to. First one you don't want to include.
+// var animals2 = animals.slice(2,4);
+// console.log(animals2)
 
-//splice - removes from array and modifies the original. start at where you want to remove, and then however many you want to remove
-animals.splice(1,1);
-console.log(animals)
+// //splice - removes from array and modifies the original. start at where you want to remove, and then however many you want to remove
+// animals.splice(1,1);
+// console.log(animals)
 
-var colors = ["red", "blue", "gold", "pink", "orange"];
+// var colors = ["red", "blue", "gold", "pink", "orange"];
 
-//sort, reverse, make a string
+// //sort, reverse, make a string
 
-var finalAnswer = colors.sort().reverse().join(" fish ");
-console.log(finalAnswer);
+// var finalAnswer = colors.sort().reverse().join(" fish ");
+// console.log(finalAnswer);
+
+//For Loops
+// for (var i = 0; i < 5; i++) {
+//   console.log(i)
 
 
+// }
+
+
+// var fruit = ["cherries", "apples", "bananas"];
+// for (var j = 0; j < fruit.length; j++) {
+//   console.log("fruit", fruit[j])
+// }
+
+// write a for loop that increments by 10 each time = 10 times 10, 20, 30....100
+
+for (var i = 0; i < 101; i+=10){
+  console.log("i", i)
+}
+
+
+// for loop that pushes the index to an array for 5 times console.log (array) [0, 1, 2, 3, 4]
+
+
+// for loop that starts by 100 and divides by 2 each time = 3 times  100,50,25
 
 
 
