8ab3c49d0f306c1d38d82fe978877e7d06349dd0
jmarcusgill <jmarcusgill@gmail.com>
2017-01-28 10:58:54 -0600

    objects and for in loops

---
+++ day3.js
@@ -0,0 +1,17 @@
+// OBJECTS
+
+var myCat = {
+  tail: "long",
+  age: 29,
+  color: "blue",
+  friends: ["zoe", "callan", "lauren"]
+}
+
+console.log("Age of cat: ", myCat.age)
+console.log("Color of cat: ", myCat["color"]);
+
+// for ( in ) loop
+for(var myKey in myCat) {
+  console.log("myKey: ", myKey)
+  console.log("values: ", myCat[myKey])
+}
